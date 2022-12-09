db48e39ad9910fab3cffe6bb4cad4f7ff5005dad
Darrell Miller <Profdm69@gmail.com>
2022-12-08 21:20:50 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include<unistd.h>
 /**
  * main - Entry point
@@ -7,8 +6,8 @@
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora	Korpar, 2015-10-19\n";
+	write(2, 
+		"and that piece of art is useful\" - Dora Korpar, 2015-			10-19\n", 59);
 
-	write(2, str1, 59);
 	return (1);
 }
