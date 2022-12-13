dfeafd5bbb2530a8c274285b368c8999a65090bf
Darrell Miller <Profdm69@gmail.com>
2022-12-13 13:52:41 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
